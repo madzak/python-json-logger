5f5073c2769c862c63340516ce59ecf66673b0ef
Zak Zajac <zak@madzak.com>
2013-03-07 06:12:28 -0800

    Updated setup.py python version classifiers

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: System :: Logging',
     ]
 )
