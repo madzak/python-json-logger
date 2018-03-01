5e75b6a67298dd45769d9433fb2868dff2d41e6d
Scott Clark <scottclark591@gmail.com>
2018-03-01 13:37:07 -0500

    Add markers for Python 3.5 and Python 3.6

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: System :: Logging',
     ]
 )
