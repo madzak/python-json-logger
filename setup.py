df4bd1062eba6cf6919ca9248005334e3f8deffa
Simeon Visser <svisser@users.noreply.github.com>
2018-06-30 23:18:28 +0100

    Only support Python 2.7, 3.4, 3.5 and 3.6
    Python 2.6, Python 3.1, Python 3.2 and Python 3.3 have reached end-of-life: https://devguide.python.org/#status-of-python-branches

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys
-if sys.version_info < (2, 6):
-    print(sys.stderr, "{}: need Python 2.6 or later.".format(sys.argv[0]))
+if sys.version_info < (2, 7):
+    print(sys.stderr, "{}: need Python 2.7 or later.".format(sys.argv[0]))
     print(sys.stderr, "Your Python is {}".format(sys.version))
     sys.exit(1)
 
@@ -25,12 +25,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
